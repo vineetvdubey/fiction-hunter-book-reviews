5656c4477176ab7f5d2d37a5393783c9262c5aef
vineetvdubey <35378726+vineetvdubey@users.noreply.github.com>
2021-06-26 19:21:15 +0530

    Added stub project files

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'client', 'build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port: ${port}`);
+  console.log('Started fiction-hunter-book-reviews service');
+});
---
+++ client/src/App.js
@@ -0,0 +1,7 @@
+import './App.css';
+
+function App() {
+  return <h1>WORK IN PROGRESS</h1>;
+}
+
+export default App;
